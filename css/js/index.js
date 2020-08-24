c1bcb3c4616f567de916c898922dbd9309904cdd
mikengugy <mikengugy@gmail.com>
2020-08-24 07:18:02 +0300

    added array of the days,male names,female names

---
+++ css/js/index.js
@@ -0,0 +1,3 @@
+var daysOfTheWeek= ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+var femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
