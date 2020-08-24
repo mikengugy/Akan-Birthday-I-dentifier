45a6fd8e17bcdaa07cfa5af3d4eb7f23bf4810c0
mikengugy <mikengugy@gmail.com>
2020-08-24 07:20:49 +0300

    add function

--- css/js/index.js
+++ css/js/index.js
@@ -1,3 +1,10 @@
 var daysOfTheWeek= ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 var femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+
+function getAkanName(){ 
+    var day = parseInt(document.getElementById("day").value);
+    var month = parseInt(document.getElementById("month").value);
+    var year = parseInt(document.getElementById("year").value);
+    var male = document.getElementById("male");
+    var female = document.getElementById("female");
