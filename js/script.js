50e056e9559c0f68e5f02226ed32d9f36028193a
mikengugy <mikengugy@gmail.com>
2020-08-24 07:50:40 +0300

    style with bootstrap

--- js/script.js
+++ js/script.js
@@ -3,19 +3,20 @@ var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 var femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 
 function getAkanName(){ 
-    var day = parseInt(document.getElementById("day").value);
-    var month = parseInt(document.getElementById("month").value);
-    var year = parseInt(document.getElementById("year").value);
-    var male = document.getElementById("male");
-    var female = document.getElementById("female");
+  var day = parseInt(document.getElementById("day").value);
+  var month = parseInt(document.getElementById("month").value);
+  var year = parseInt(document.getElementById("year").value);
+  var male = document.getElementById("male");
+  var female = document.getElementById("female");
+ 
+  if(day <=0 || day >31){
+      alert("Please enter valid day")
+  }
+  else if(month <=0 || month >12 || (month == 2 && day > 29)){
+     alert("Please enter valid month") 
+  }
 
-    if(day <=0 || day >31){
-        alert("Please enter valid day")
-    }
-    else if(month <=0 || month >12 || (month == 2 && day > 29)){
-       alert("Please enter valid month") 
-    }
-    var a = Math.floor((14 - month) / 12)
+  var a = Math.floor((14 - month) / 12)
   var y = year - a
   var m = month + 12 * a - 2
   var d = (day + y + Math.floor(y / 4) - Math.floor(y / 100) + Math.floor(year / 400) + Math.floor((31 * m )/ 12)) % 7;
